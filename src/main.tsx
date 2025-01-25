e64f7da9720959dcf678a2557cb10e9566ee5e04
Ryuusei1017 <ryuusei1017168@gmail.com>
2025-01-25 17:49:27 +0900

    Resolved merge conflict and kept MindMap/Register component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 // src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< Updated upstream
-import './components/Register.css';
-import Register from './components/Register.tsx'; // Registerコンポーネントをインポート
-import './globals.css';
-=======
+
 import './components/mindmap.css';  // mindmapのスタイルをインポート
 import MindMap from './components/mindmap'; // MindMapコンポーネントをインポート
->>>>>>> Stashed changes
+
 
 const root = createRoot(document.getElementById('root')!);
 
