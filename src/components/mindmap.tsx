dc87420c3ddeeff64ae61c29c7166dbc7abbf9cd
Takagi Tasuku <tasuren@outlook.jp>
2025-02-25 16:20:41 +0900

    Forceレイアウトに変更し、アイデアとタグ管理を実装

--- src/components/mindmap.tsx
+++ src/components/mindmap.tsx
@@ -1,19 +1,90 @@
 import { SigmaContainer } from "@react-sigma/core";
-import { useWorkerLayoutForceAtlas2 } from "@react-sigma/layout-forceatlas2";
+import { useWorkerLayoutForce } from "@react-sigma/layout-force";
 import { MultiDirectedGraph } from "graphology";
 import { useEffect } from "react";
+import type { NodeHoverDrawingFunction } from "sigma/rendering";
+import { Idea, Tag } from "../libs/model";
 import AddIdeaButton from "./AddIdeaButton";
 
-function FA2() {
-	const { start, kill } = useWorkerLayoutForceAtlas2({
-		settings: { slowDown: 10 },
+class MindMapController {
+	graph: MultiDirectedGraph;
+	private ideas: Map<number, Idea>;
+	private tags: Map<number, Tag>;
+
+	constructor() {
+		this.graph = new MultiDirectedGraph();
+		this.ideas = new Map();
+		this.tags = new Map();
+	}
+
+	makeNodeName(obj: Idea | Tag): string {
+		let prefix = null;
+
+		if (obj instanceof Idea) {
+			prefix = "idea";
+		}
+
+		if (obj instanceof Tag) {
+			prefix = "tag";
+		}
+
+		if (obj) return `${prefix}-${obj.id}`;
+
+		throw new Error("指定されたオブジェクトにはID生成に対応していません。");
+	}
+
+	addIdea(idea: Idea) {
+		this.ideas.set(idea.id, idea);
+
+		const tags = idea.tags.map((tag) => `#${tag}`).join(" ");
+		this.graph.addNode(idea.node, {
+			label: `${idea.name}\n${tags}`,
+			size: 50,
+			x: Math.random() * 10,
+			y: Math.random() * 10,
+		});
+	}
+
+	removeIdea(idea: Idea) {
+		this.ideas.delete(idea.id);
+		this.graph.dropNode(idea.node);
+	}
+
+	addTag(tag: Tag) {
+		this.tags.set(tag.id, tag);
+
+		this.graph.addNode(tag.node, {
+			label: `#${tag.name}`,
+			size: 50,
+			x: Math.random() * 10,
+			y: Math.random() * 10,
+		});
+	}
+
+	removeTag(tag: Tag) {
+		this.tags.delete(tag.id);
+		this.graph.dropNode(tag.node);
+	}
+
+	/** 指定されたタグとそのタグが付いたアイデアをマインドマップに追加する。 */
+	addTagAndIdeas(tag: Tag, ideas: Idea[]) {
+		this.addTag(tag);
+
+		for (const idea of ideas) {
+			this.addIdea(idea);
+			this.graph.addDirectedEdge(tag.node, idea.node);
+		}
+	}
+}
+
+function Force() {
+	const { start, kill } = useWorkerLayoutForce({
+		settings: {},
 	});
 
 	useEffect(() => {
-		// start FA2
 		start();
 
-		// Kill FA2 on unmount
 		return () => {
 			kill();
 		};
@@ -23,16 +94,28 @@ function FA2() {
 }
 
 function MindMap() {
-	const graph = new MultiDirectedGraph();
+	const controller = new MindMapController();
 
-	graph.addNode("a1", { label: "Node 1", size: 30, x: 1, y: 0 });
-	graph.addNode("a2", { label: "Node 2", size: 30, x: 0, y: 0 });
-	graph.addNode("a3", { label: "Node 3", size: 30, x: 2, y: 3 });
+	controller.addTagAndIdeas(new Tag("ブルーアーカイブ", 1), [
+		new Idea("天使の輪っか 変える", 1, [], "天使の輪っかの説明"),
+		new Idea("テスト2", 2, [], "テスト2の説明"),
+		new Idea("テスト3", 3, [], "テスト3の説明"),
+	]);
+
+	const defaultDrawNodeHover: NodeHoverDrawingFunction = (
+		ctx,
+		data,
+		settings,
+	) => {};
 
 	return (
 		<div>
-			<SigmaContainer graph={graph} style={{ width: "100%", height: "100vh" }}>
-				<FA2 />
+			<SigmaContainer
+				graph={controller.graph}
+				style={{ width: "100%", height: "100vh" }}
+				settings={{ labelColor: { color: "white" }, defaultDrawNodeHover }}
+			>
+				<Force />
 			</SigmaContainer>
 			<AddIdeaButton /> {/* 右下のボタンを追加 */}
 		</div>
--- src/libs/model.tsx
+++ src/libs/model.tsx
@@ -1,6 +1,31 @@
-export interface Idea {
-	name: string;
-	id: number;
-	tags: string;
-	description: string;
+export abstract class Nodeable {
+	abstract get node(): string;
+}
+
+export class Tag extends Nodeable {
+	constructor(
+		public name: string,
+		public readonly id: number,
+	) {
+		super();
+	}
+
+	override get node() {
+		return `tag-${this.id}`;
+	}
+}
+
+export class Idea extends Nodeable {
+	constructor(
+		public name: string,
+		public readonly id: number,
+		public tags: Tag[],
+		public description: string,
+	) {
+		super();
+	}
+
+	override get node() {
+		return `idea-${this.id}`;
+	}
 }
