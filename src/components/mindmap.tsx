6a8ed0dbe915dba89960691bea4b0bacc704f616
Takagi Tasuku <tasuren@outlook.jp>
2025-02-25 15:12:13 +0900

    ForceAtlas2レイアウトで点を自動配置するよう実装

--- src/components/mindmap.tsx
+++ src/components/mindmap.tsx
@@ -1,28 +1,38 @@
-import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
+import { SigmaContainer } from "@react-sigma/core";
+import { useWorkerLayoutForceAtlas2 } from "@react-sigma/layout-forceatlas2";
 import { MultiDirectedGraph } from "graphology";
 import { useEffect } from "react";
 import AddIdeaButton from "./AddIdeaButton";
 
-function LoadGraph() {
-	const loadGraph = useLoadGraph();
+function FA2() {
+	const { start, kill } = useWorkerLayoutForceAtlas2({
+		settings: { slowDown: 10 },
+	});
 
 	useEffect(() => {
-		const graph = new MultiDirectedGraph();
+		// start FA2
+		start();
 
-		graph.addNode("a1", { label: "Node 1", size: 30, x: 0, y: 0 });
-		graph.addNode("a2", { label: "Node 2", size: 30, x: 10, y: 10 });
-
-		loadGraph(graph);
-	}, [loadGraph]);
+		// Kill FA2 on unmount
+		return () => {
+			kill();
+		};
+	}, [start, kill]);
 
 	return null;
 }
 
 function MindMap() {
+	const graph = new MultiDirectedGraph();
+
+	graph.addNode("a1", { label: "Node 1", size: 30, x: 1, y: 0 });
+	graph.addNode("a2", { label: "Node 2", size: 30, x: 0, y: 0 });
+	graph.addNode("a3", { label: "Node 3", size: 30, x: 2, y: 3 });
+
 	return (
 		<div>
-			<SigmaContainer style={{ width: "100%", height: "100vh" }}>
-				<LoadGraph />
+			<SigmaContainer graph={graph} style={{ width: "100%", height: "100vh" }}>
+				<FA2 />
 			</SigmaContainer>
 			<AddIdeaButton /> {/* 右下のボタンを追加 */}
 		</div>
